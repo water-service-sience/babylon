2f84adbee840d899c0f4e1a713642fd196f4a4b1
takezoux2 <takeshita@geishatokyo.com>
2013-09-10 19:12:28 +0900

    Set image cache expire.

--- babylon-server/app/controllers/Application.scala
+++ babylon-server/app/controllers/Application.scala
@@ -3,6 +3,8 @@ package controllers
 import play.api._
 import play.api.mvc._
 import controllers.manager.PhotoManager
+import java.util.Date
+import java.text.SimpleDateFormat
 
 object Application extends Controller {
   
@@ -13,7 +15,18 @@ object Application extends Controller {
   def photo(path : String) = Action {
     val photo = PhotoManager.getFileData(path)
 
-    Ok(photo).as("image/jpeg")
+    val format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz")
+    val expireSecs = 60 * 60 * 24 * 30 //キャッシュはおよそ１ヶ月
+    val lastModified = new Date(new Date().getTime - 1000)
+    val expireTime = new Date(lastModified.getTime + expireSecs * 1000)
+
+
+    //header('Last-Modified: Fri Jan 01 2010 00:00:00 GMT');
+
+    Ok(photo).as("image/jpeg").withHeaders(
+      "Last-Modified" -> format.format(lastModified),
+      "Expires" -> format.format(expireTime),
+      "Cache-Control" -> ("private, max-age=" + expireSecs))
   }
 
 }
