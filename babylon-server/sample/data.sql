167f77dda8ae2e26483d6813f4930193610cf6da
Takeshita Yoshiteru <takezoux2@gmail.com>
2014-06-02 03:12:38 +0900

    グラフの横幅を調整

---
+++ babylon-server/sample/data.sql
@@ -0,0 +1,4 @@
+insert into waterlevelfield (id,fieldRouter,sensorName,timestampColumnName,sensorColumnName,valueFactor,valueOffset) values
+(1,1,"HandaW1","Timestamp","Port2","-0.0354","52.368"),
+(2,1,"HandaW2","Timestamp","Port2","-0.0302",47.065),
+(3,1,"HandaMet","Timestamp","Port5", -0.0313,48.421);
