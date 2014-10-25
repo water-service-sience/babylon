909a67934dee6ff43c51bc675fecbfc8e923f5f6
takeshita <takezoux2@gmail.com>
2014-10-26 03:56:49 +0900

    取得部分を修正

--- babylon-library/src/main/scala/jp/utokyo/babylon/db/WaterLevel.scala
+++ babylon-library/src/main/scala/jp/utokyo/babylon/db/WaterLevel.scala
@@ -1,5 +1,6 @@
 package jp.utokyo.babylon.db
 
+import java.text.SimpleDateFormat
 import java.util.Date
 import net.liftweb.mapper._
 import jp.utokyo.babylon.util.TimeUtil
@@ -16,8 +17,8 @@ object WaterLevel extends WaterLevel with LongKeyedMetaMapper[WaterLevel]{
     val oldest = new Date(values.map(_._1.getTime).min)
     bulkDelete_!!(By(waterLevelFieldId,f.id.get),By_>=(recordTime,oldest))
     // 45日以上前のデータも消す
-    bulkDelete_!!(By(waterLevelFieldId,f.id.get),By_<(recordTime,
-      new Date(new Date().getTime - TimeUnit.DAYS.toMillis(45))))
+    /*bulkDelete_!!(By(waterLevelFieldId,f.id.get),By_<(recordTime,
+      new Date(new Date().getTime - TimeUnit.DAYS.toMillis(45))))*/
     values.foreach(v => {
       val wl = createInstance
       wl.waterLevelFieldId := f.id.get
@@ -56,11 +57,23 @@ object WaterLevel extends WaterLevel with LongKeyedMetaMapper[WaterLevel]{
     }
   }
   def getDataListAfter(f : WaterLevelField,pivot : Date,maxRecords : Int) = {
+    // 一時的に、8月台の日付を強制的に取るようにする
+
+    val diff = new Date().getTime - pivot.getTime
+    val end = new SimpleDateFormat("yyyy-MM-dd").parse("2014-08-01")
+
+    val start = new Date(end.getTime - diff)
     val l = findAll(
+      By(waterLevelFieldId,f.id.get),
+      By_>=(recordTime,start),
+      By_<=(recordTime,end),
+      OrderBy(recordTime,Descending)).reverse
+
+    /*val l = findAll(
       By(waterLevelFieldId,f.id.get),
       By_>=(recordTime,pivot),
       OrderBy(recordTime,Descending)).reverse
-
+    */
     val g = l.size / maxRecords
     if(g <= 1) l
     else {
